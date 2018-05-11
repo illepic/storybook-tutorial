176f2e15b8209a8e4d0017ca6acb6a8ba739927b
Christopher Bloom <bloomcb@gmail.com>
2018-05-11 10:24:45 -0700

    Initial commit for presentation

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Provider } from 'react-redux';
+
+import { PureInboxScreen } from './InboxScreen';
+import { defaultTasks } from './TaskList.stories';
+
+// A super-simple mock of a redux store
+// Bloom note: literally mocking each thing a redux store would do. Wow.
+const store = {
+  getState: () => {
+    return {
+      tasks: defaultTasks,
+    };
+  },
+  subscribe: () => 0,
+  dispatch: action('dispatch'),
+};
+
+storiesOf('InboxScreen', module)
+  .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add('default', () => <PureInboxScreen />)
+  .add('error', () => <PureInboxScreen error="Something" />);
